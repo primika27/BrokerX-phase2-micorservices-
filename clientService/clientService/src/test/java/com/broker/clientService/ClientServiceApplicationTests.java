f91faeaae98c2c0aaf282c0fcc15dab6171f3d4f
primika27 <kprimika@yahoo.com>
2025-10-28 15:16:53 -0400

    Fix ClientService test configuration - add SpringBootApplication reference and test profile

--- clientService/clientService/src/test/java/com/broker/clientService/ClientServiceApplicationTests.java
+++ clientService/clientService/src/test/java/com/broker/clientService/ClientServiceApplicationTests.java
@@ -2,12 +2,15 @@ package com.broker.clientService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = ClientServiceApplication.class)
+@ActiveProfiles("test")
 class ClientServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Test that the application context loads successfully
 	}
 
 }
